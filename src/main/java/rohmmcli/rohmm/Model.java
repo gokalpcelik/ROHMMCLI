e53e1c0a9bea6965a6ff5a5be8ee5d663e083cb1
denemey <deneme@gmail.com>
2020-01-21 10:24:12 +0300

    Model.java

---
+++ src/main/java/rohmmcli/rohmm/Model.java
@@ -0,0 +1,19 @@
+package rohmmcli.rohmm;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Model
+{
+	public Model(String type)
+	{
+
+	}
+
+	public Model(File modelfile) throws FileNotFoundException
+	{
+
+	}
+
+	
+}
