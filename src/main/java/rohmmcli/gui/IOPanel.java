96bac300f6d4a1b0439745cee30907aad7072328
kelepiradam <gokalpceliksentegen@gmail.com>
2020-05-03 14:29:48 +0300

    IOPanel - OptionPanel

---
+++ src/main/java/rohmmcli/gui/IOPanel.java
@@ -0,0 +1,14 @@
+package rohmmcli.gui;
+
+import javax.swing.JPanel;
+
+public class IOPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public IOPanel() {
+
+	}
+
+}
---
+++ src/main/java/rohmmcli/gui/OptionPanel.java
@@ -0,0 +1,14 @@
+package rohmmcli.gui;
+
+import javax.swing.JPanel;
+
+public class OptionPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public OptionPanel() {
+
+	}
+
+}
